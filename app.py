c4deacfec48ef3738cf27f77d9c0ab5814b7135d
EmilyMunozR <emilymunoz1018@gmail.com>
2025-10-15 17:11:50 -0600

    Update app.py
    Otra vez dashboard

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ def landingPage():
 # Te regresa a (index)
 @app.route("/dashboard")
 def dashboard():
-    return render_template("login.html")
+    return render_template("dashboard.html")
 
 @app.route("/login")
 def appLogin():
@@ -720,6 +720,7 @@ def cargarIntegrantes():
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
+
 
 
 
