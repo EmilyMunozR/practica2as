197f5db924e0407e216abebcb0c1f492ac205cc3
EmilyMunozR <emilymunoz1018@gmail.com>
2025-10-15 20:32:40 -0600

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -163,8 +163,7 @@ app.controller("appCtrl", function ($scope, $http, $rootScope, $location) {
                     localStorage.setItem("login", "1");
                     localStorage.setItem("preferencias", JSON.stringify(respuesta.usuario || {}));
                     $("#frmInicioSesion")[0].reset();
-                    window.location.href = "/dashboard#/integrantes
-
+                    window.location.href = "/dashboard"
                 } else {
                     pop(".div-inicio-sesion", "Usuario y/o contraseña incorrectos", "danger");
                 }
@@ -702,8 +701,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     activeMenuOption(location.hash);
 });
-
-
 
 
 
